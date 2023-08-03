4d8c335c01d41e90020f4e9127ff96006b128c98
none <none>
2023-08-02 20:11:02 -0700

    Added python3.11 to setup

--- setup_ansible.sh
+++ setup_ansible.sh
@@ -3,7 +3,7 @@
 # This script is made to setup and configure ansible to handle and manage windows hosts via winrm
 sudo update-ca-certificates
 sudo apt update -y && apt upgrade -y
-sudo update-ca-certificates
-pip install -U ansible
+sudo apt install python3.11
+pip3 install -U ansible
 ansible-galaxy collection install ansible.windows
-pip install -U pywinrm
+pip3 install -U pywinrm
